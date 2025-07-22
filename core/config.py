764bc103178fe866179a843b0735a7cb2ef52da2
Mohammad Mahdi Takfallah <mmtmmt945@gmail.com>
2025-07-22 14:55:11 +0330

    halfway there
    halfway there

--- core/config.py
+++ core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DEBUG: bool = True
-    POSTGRESQL_DATABASE_URI: str = 'sqlite:///sqlite.db'
+    POSTGRESQL_DATABASE_URI: str = "postgres://postgres:postgres@db/postgres"
     JWT_SECRET_KEY: str = 'jwt-secret-key'
 
 
